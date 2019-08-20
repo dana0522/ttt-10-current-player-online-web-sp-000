577a4606c357b3c9a53c8b11b9e48dcc13fb71de
dana kim <dana0522@live.com>
2019-08-20 02:39:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,7 +13,7 @@ end
 def current_player(board)
 	if turn_count(board)==0
 	  return "X"
-  elsif integer.(turn_count(board)) % 2 ==0
+  elsif Integer.(turn_count(board)) % 2 ==0
     return "O"
   else return "X"
   end
